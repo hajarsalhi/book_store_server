63deeb1bdae7911d6e835f7a817b6c0cee19c036
hajarsalhi <shajar32@gmail.com>
2025-01-16 11:13:48 +0100

    add prod database url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI_LOCAL || process.env.MONGODB_URI_PROD);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
